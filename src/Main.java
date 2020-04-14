0b35bda3924a8ad28ba1a50d1816df09a9d39441
mrty2502 <61007059+mrty2502@users.noreply.github.com>
2020-04-14 09:59:11 +0700

    add file racing-cars

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static int DISTANCE = 100;
+    public static int STEP = 2;
+
+    public static void main(String[] args) {
+        Car car1 = new Car("BMW X7");
+        Car car2 = new Car("Audi Q7");
+        Car car3 = new Car("Mercedes GLS 450");
+
+        Thread thread1 = new Thread(car1);
+        Thread thread2 = new Thread(car2);
+        Thread thread3 = new Thread(car3);
+
+        System.out.println("Distance: 100KM");
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
